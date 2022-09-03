35cb3b4ba2375758ce7f02133c5fa95efef9c675
Ivo Schwizer <ivoschwizer@gmail.com>
2022-09-03 21:03:31 +0200

    n

--- nav.php
+++ nav.php
@@ -1,9 +1,10 @@
 <header>
-  <a href="index">SchwizerDesign</a>
+  <h1 class="logo"><a href="index">SchwizerDesign</a></h1>
+  <nav>
+    <ul>
+      <li id="projekte"><a href="projekte">Projekte</a></li>
+      <li id="studio"><a href="studio">Studio</a></li><li></li>
+    </ul>
+  </nav>
+
 </header>
-<nav>
-  <ul>
-    <li id="projekte"><a href="projekte">Projekte</a></li>
-    <li id="studio"><a href="studio">Studio</a></li><li></li>
-  </ul>
-</nav> 
--- projekte.php
+++ projekte.php
@@ -1,1 +1,12 @@
-test
+<!doctype html>
+<html class="no-js" lang="">
+
+<?php include 'head.php'; ?>
+
+<body>
+  <?php include 'nav.php'; ?>
+  <!-- Add your site or application content here -->
+  <div class="backgroundtest"></div>
+</body>
+
+</html>
