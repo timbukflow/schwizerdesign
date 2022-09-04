675784a1aa6ae8b018f0bbcaa64a1e7f6dc40228
Ivo Schwizer <ivoschwizer@gmail.com>
2022-09-04 15:23:12 +0200

    s

--- projekte.php
+++ projekte.php
@@ -7,7 +7,7 @@
   <?php include 'js-script.php'; ?>
   <?php include 'nav.php'; ?>
   <main >
-    <img id="swup" class="transition-fade" src="img/test.jpg" alt="test">
+    <img class="testbild" src="img/test.jpg" alt="test">
   </main>
 
 
