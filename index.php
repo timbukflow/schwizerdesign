cfa51503ebb43194a9664c6fb29110c2419e1669
Ivo Schwizer <ivoschwizer@gmail.com>
2022-09-04 13:34:51 +0200

    s

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 
 <body>
   <?php include 'nav.php'; ?>
+  <?php include 'js-script.php'; ?>
   <main id="swup" class="transition-fade">
     <h1>this is the homepage</h1>
   </main>
@@ -12,7 +13,7 @@
   <div class="backgroundtesthome"></div>
 
 
-  <?php include 'js-script.php'; ?>
+
 </body>
 
 </html>
--- projekte.php
+++ projekte.php
@@ -4,12 +4,13 @@
 <?php include 'head.php'; ?>
 
 <body>
+  <?php include 'js-script.php'; ?>
   <?php include 'nav.php'; ?>
   <main id="swup" class="transition-fade">
     <h1>this is the page</h1>
   </main>
   <div class="backgroundtest"></div>
-  <?php include 'js-script.php'; ?>
+
 </body>
 
 </html>
