eeb0fb29e19da9655e6b545880f16a9b5912b156
Ivo Schwizer <ivoschwizer@gmail.com>
2022-09-04 15:58:38 +0200

    t

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
   <?php include 'nav.php'; ?>
   <?php include 'js-script.php'; ?>
   <main id="swup" class="transition-fade" >
-      <a  href="/projekte"><img src="img/test.jpg" alt="test"></a>
+      <a  href="/projekte"><img class="img01" src="img/test.jpg" alt="test"></a>
+      <a  href=""><img class="img02" src="img/ddd.jpg" alt="adf"></a>
   </main>
 
 
