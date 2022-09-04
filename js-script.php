45aeebb22fdc2c6ea84702672b4847bdc1fa14a6
Ivo Schwizer <ivoschwizer@gmail.com>
2022-09-04 13:32:43 +0200

    s

--- js-script.php
+++ js-script.php
@@ -1,7 +1,8 @@
 <script src="js/vendor/modernizr-3.11.2.min.js"></script>
 <script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
 <script defer src="js/swup-min.js"></script>
+<script defer src="js/main.js"></script>
+
 
 <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 <script>
