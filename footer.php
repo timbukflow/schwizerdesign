8d396d0eef4cc2d230995aa8116efe3f8502a7d2
Ivo Schwizer <ivoschwizer@gmail.com>
2022-12-24 10:06:03 +0100

    ralph

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <div class="footerfirst">
+            <p>Davidstrasse 45 <br> 9000 St.Gallen</p>
+            <a href="tel:+41765280850">Tel 076 528 08 50</a>
+            <a href="mailto:cuomo@locherfood.ch" target="_top">ivo@schwizerdesign.ch</a>
+            <p>Copyright © 2023<br>SchwizerDesign GmbH <br>All Rights Reserved</p>
+        </div>
+        <div class="footerSecond">
+            <a href="https://www.linkedin.com/in/ivo-schwizer-a048685b/" target="_blank">linkedin</a>
+            <a href="https://www.definieren.ch/" target="_blank">instagram</a>
+        </div> 
+    </footer>
