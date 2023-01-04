23baf20d15cd37b55b8c19a0be5d717fe8bd5100
Ivo Schwizer <ivoschwizer@gmail.com>
2023-01-04 20:22:46 +0100

    delit transition

--- maintran.js
+++ maintran.js
@@ -1,21 +1,21 @@
-window.onload = () => {
-    const links = document.querySelectorAll('a');
-    const transition = document.querySelector('.transition');
+// window.onload = () => {
+//     const links = document.querySelectorAll('a');
+//     const transition = document.querySelector('.transition');
   
-    setTimeout(() => {
-      transition.classList.remove('is-active');
-    }, 750);
+//     setTimeout(() => {
+//       transition.classList.remove('is-active');
+//     }, 750);
   
-    for (let i = 0; i < links.length; i++) {
-      const link = links[i];
+//     for (let i = 0; i < links.length; i++) {
+//       const link = links[i];
   
-      link.addEventListener('click', e => {
-        e.preventDefault();
-        let target = e.target.href;
-        transition.classList.add('is-active');
-        setInterval(() => {
-          window.location.href = target;
-        }, 750);
-      })
-    }
-  }
+//       link.addEventListener('click', e => {
+//         e.preventDefault();
+//         let target = e.target.href;
+//         transition.classList.add('is-active');
+//         setInterval(() => {
+//           window.location.href = target;
+//         }, 750);
+//       })
+//     }
+//   }
--- nav.php
+++ nav.php
@@ -3,6 +3,3 @@
     <a class="linkStart" href="index.html">SchwizerDesign</a>
     <a class="linkAbout" href="info">Info</a>
 </nav>
-   
-<!-- Page Transition -->
-<div class="transition transition-3 is-active"></div>
